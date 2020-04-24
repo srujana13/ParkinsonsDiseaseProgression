da7b5997d11fc387f6a1a0cc3e7e168345fbc00b
RJAIN-27 <56007413+RJAIN-27@users.noreply.github.com>
2020-04-24 11:50:15 -0400

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -6,8 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 import preprocessing
 
-learning_rate=0.04
-epochs=200
+learning_rate=0.0004
+epochs=5000
 MAE_val=[]
 
 def forward_propogation(X_input,weights,b):
@@ -23,10 +23,10 @@ def back_propogation(X,y,z):
 def gradient_descent(w,b,dw,db,learning_rate):
     return (w-learning_rate*dw), (b-learning_rate*db)
 
-def linear_regression(X_train,y_train,X_val,y_val,learning_rate,epochs):
+def linear_regression(X_train,y_train,X_test,y_test,learning_rate,epochs):
     length_w=X_train.shape[0]
     w,b=np.random.randn(1, length_w),0
-    rows_training,rows_validation=y_train.shape[1],y_val.shape[1]
+    rows_training,rows_validation=y_train.shape[1],y_test.shape[1]
 
     for i in range(epochs):
         z_train=forward_propogation(X_train, w, b)
@@ -35,19 +35,20 @@ def linear_regression(X_train,y_train,X_val,y_val,learning_rate,epochs):
         w,b=gradient_descent(w,b,dw,db,learning_rate)
         training_MAE=(float(1)/float(rows_training))*np.sum(np.abs(z_train-y_train))
 
-        z_val=forward_propogation(X_val, w, b)
-        validation_cost=cost(z_val, y_val)  
-        validation_MAE=(float(1)/float(rows_validation))*np.sum(np.abs(z_val-y_val))
+        z_test=forward_propogation(X_test, w, b)
+        validation_cost=cost(z_val, y_test)  
+        validation_MAE=(float(1)/float(rows_validation))*np.sum(np.abs(z_test-y_test))
         MAE_val.append(validation_MAE)
 
-    return training_MAE, validation_MAE, np.sqrt(((z_val - y_val) ** 2).mean())
+    return training_MAE, validation_MAE, np.sqrt(((z_test - y_test) ** 2).mean())
 
-X_train, X_val, y_train, y_val = preprocessing.load_data('./parkinsons_updrs.csv')
-X_train, X_val = preprocessing.random_forest_features(X_train, y_train, X_val)
-X_train, X_val=X_train.T, X_val.T
-y_train, y_val=np.array([y_train]), np.array([y_val])
+X_train, X_test, y_train, y_test = preprocessing.load_data('./parkinsons_updrs.csv')
+X_train, X_test = preprocessing.random_forest_features(X_train, y_train, X_test)
+#X_train, X_test = preprocessing.pca_features(X_train, X_test)
+X_train, X_test=X_train.T, X_test.T
+y_train, y_test=np.array([y_train]), np.array([y_test])
 
-training_MAE, validation_MAE, RMSE=linear_regression(X_train, y_train, X_val, y_val, learning_rate, epochs)
+training_MAE, validation_MAE, RMSE=linear_regression(X_train, y_train, X_test, y_test, learning_rate, epochs)
 
 print ("learning_rate "+str(learning_rate)+" epochs "+str(epochs))
 print("Training MAE")
@@ -60,14 +61,14 @@ print (RMSE)
 
 linear_regression=linear_model.LinearRegression()
 model=linear_regression.fit(X_train.T, y_train.T)
-predictions=linear_regression.predict(X_val.T)
+predictions=linear_regression.predict(X_test.T)
 
 print ("MAE with the Sklearn library")
-MAE_with_library=(1.0/y_val.shape[1])*np.sum(np.abs(predictions-y_val.T))
+MAE_with_library=(1.0/y_test.shape[1])*np.sum(np.abs(predictions-y_test.T))
 print(MAE_with_library)
 print ("RMSE with the SKlearn library")
-RMSE_with_library=(1.0/y_val.shape[1])*np.sum(np.abs(predictions-y_val.T))
-print(np.sqrt(((predictions - y_val) ** 2).mean()))
+RMSE_with_library=(1.0/y_test.shape[1])*np.sum(np.abs(predictions-y_test.T))
+print(np.sqrt(((predictions - y_test) ** 2).mean()))
 
 
 plt.plot(MAE_val)
